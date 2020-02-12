3438b9dd5aa94441ddd94578ed3d026721d7cabd
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-02-12 12:56:18 +0100

    Mute output of stanfordnlp

--- stanfordnlp_wrapper/__main__.py
+++ stanfordnlp_wrapper/__main__.py
@@ -1,6 +1,7 @@
 import logging
 import argparse
 import sys
+import io
 
 from . import parse, __version__
 
@@ -16,10 +17,15 @@ parser = argparse.ArgumentParser(description='Morphosyntactic parser based on St
 parser.add_argument("--verbose", "-v", help="Verbose output", action="store_true")
 parser.add_argument('-V', '--version', action='version', version="{} ({})".format(__name__, __version__))
 
+
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
                     format='[%(asctime)s %(name)-12s %(levelname)-5s] %(message)s')
 
+# Mute output
+stdout = io.StringIO()
+sys.stdout = stdout
 in_obj = parse(input_file)
+sys.stdout = sys.__stdout__
 in_obj.dump()
